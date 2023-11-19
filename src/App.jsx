dd094fffcb33bba8980755ecd995f5800e99ee2f
Akif Albayrak <akifalbayrak6161@gmail.com>
2023-11-19 17:46:28 +0300

    Update app.jsx
    adding signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from './components/Header.jsx';
+import Signup from './components/Signup.jsx';
 import Login from './components/StateLogin.jsx';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
       <Header />
       <main>
         <Login />
+        <Signup/>
       </main>
     </>
   );
